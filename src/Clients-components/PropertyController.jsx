26f2a2210b9a44ce0fbf9e8e2ec4a439f00f705e
shyam <prabhashyams@gmail.com>
2026-02-10 17:39:26 +0530

    update code

--- src/Clients-components/PropertyController.jsx
+++ src/Clients-components/PropertyController.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { getAuthToken } from '../utils/tokenUtils';
 
-
 //export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.livyco.com';
 
